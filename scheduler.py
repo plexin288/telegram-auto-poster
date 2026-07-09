b18c448f2eb18821ab8ce2ad6cd5f22ad7a4bcc5
plexin288 <rennoglory@gmail.com>
2026-07-09 10:57:18 +0700

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,24 +1,11 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-from config import Config
 from services import PostService
-
-
-scheduler = AsyncIOScheduler(
-    timezone=Config.TIMEZONE
-)
 
 service = PostService()
 
 
 async def run_scheduler():
-    scheduler.add_job(
-        service.process_posts,
-        "interval",
-        seconds=30,
-        id="telegram_auto_post"
-    )
+    print("Checking scheduled posts...")
 
-    scheduler.start()
+    await service.process_posts()
 
-    print("✅ Scheduler Started (30 seconds)")
+    print("Done.")
